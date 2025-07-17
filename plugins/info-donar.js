9f1a2a62155949ca0aab47dfa115d7af81cdacaf
the-27 <blackoficial2025@gmail.com>
2025-07-16 20:54:10 -0500

    𝖘𝖚𝖐𝖚𝖓𝖆  𝕸𝕯

---
+++ plugins/info-donar.js
@@ -0,0 +1,37 @@
+let media = './media/menus/Menu4paypal.jpg'
+let handler = async (m, { conn, command }) => {
+let user = db.data.users[m.sender]
+let text = `Hola y Adios
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+${bot}
+┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
+await conn.sendButton(m.chat, text, `𝗦𝗛𝗔𝗗𝗢𝗪'𝗖𝗢𝗥𝗘` + wm, media, [
+['ɢʀᴜᴘᴏs', '.grupos'],
+['ᴏᴡɴᴇʀ', '#owner'],
+['ᴍᴇɴᴜ ᴏғᴄ', '/menu']], null, [
+['ɢɪᴛʜᴜʙ', `${global.md}`]], fkontak)}
+/*conn.sendMessage(m.chat, { 
+text: text, 
+contextInfo:{
+forwardingScore: 9999999,
+isForwarded: true, 
+mentionedJid:[m.sender],
+"externalAdReply": {
+"showAdAttribution": true,
+"renderLargerThumbnail": true,
+"thumbnail": imagen1, 
+"title": `ᴘᴏᴡᴇʀᴇᴅ ʙʏ sʜᴀᴅᴏᴡ'ᴄᴏʀᴇ`, 
+body: bot, 
+"containsAutoReply": true,
+"mediaType": 1, 
+"mediaUrl": redes, 
+"sourceUrl": redes, 
+}}}, { quoted: fkontak })}*/
+/*conn.sendHydrated(m.chat, text, `𝐒𝐇𝐀𝐃𝐎𝐖'𝐂𝐎𝐑𝐄 𝐁𝐘 𝐒𝐔𝐊𝐔𝐍𝐀 𝐌𝐃` + wm, media, 'https://github.com/Yuji-XDev', 'ɢɪᴛᴜʜʙ', null, null, [
+['ɢʀᴜᴘᴏs', '.grupos'],
+['ᴄʀᴇᴀᴅᴏʀ', '#owner'],
+['ᴍᴇɴᴜ', '/menu']
+], m,)}*/
+handler.command = ['Hl999']
+handler.exp = 80
+export default handler
